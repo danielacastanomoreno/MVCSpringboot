6dadbe569ece6bb137d674374d84bff3f7bb5873
DanielaCastano <1113979182@u.icesi.edu.co>
2025-09-18 16:01:17 -0500

    Class of today, update Daniela

--- src/main/java/org/example/introspringboot/controller/StudentController.java
+++ src/main/java/org/example/introspringboot/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,7 +25,19 @@ public class StudentController {
     public String getStudents(Model model) {
         List<Student> studentsList = studentService.getStudents();
         model.addAttribute("studentsList", studentsList);
+
+        Student student = new Student();
+        model.addAttribute("student", student);
         return "student/students-list";
+    }
+
+    @PostMapping("/save")
+    // Asi recibimos los cascarones llenados
+    public String saveStudent(@ModelAttribute Student student) {
+
+        studentService.save(student);
+        return "redirect:/students/";
+
     }
 
 }
